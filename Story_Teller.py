01fcadf3ee1bbbf163b512bd2e756f0c2cae2d3f
RyzenMu <muruga13in2022@gmail.com>
2022-09-25 14:00:08 +0530

    Story Teller comments

---
+++ Story_Teller.py
@@ -0,0 +1,5 @@
+# Did you think the most complex way of using a random module in python is random sampling? This idea cannot be farther from the truth. It is also possible to create random stories and even beyond using the random module. 
+
+# Here, the aim is to create a random story each time the user runs the program. The story will be the same always but with little variation with the input. This is a fun but exciting python project which will work wonders with beginners to gain confidence in python.
+
+# In a nutshell, the program will ask users for inputs such as the name of a place, action, etc. and then build a story around the data.  
