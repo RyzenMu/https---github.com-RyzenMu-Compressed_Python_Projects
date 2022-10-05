76324749b7214c51e4e3dddff848e0f6d00308f1
RyzenMu <muruga13in2022@gmail.com>
2022-10-05 19:08:15 +0530

    17 basic tkinter elements completed

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,3 +1,5 @@
+from doctest import master
+from logging import root
 from tkinter import *
 import tkinter as tk
 
@@ -111,20 +113,111 @@ def listboxInTinker():
     Lb.pack()
     top.mainloop()
 
-#menu button in Tkinter
-def menuButtonInTinker():
-    top = Tk()
-    mb = Menubutton( top, text = &quot;GfG&quot;)
-    mb.grid()
-    mb.menu = Menu ( mb, tearoff=0)
-    mb[&quot;menu&quot;] = mb.menu
-    cVar = IntVar()
-    aVar = IntVar()
-    mb.menu.add_checkbutton(label='Contact', variable=cVar)
-    mb.menu.add_checkbutton(label='About', variable=aVar)
-    mb.pack()
+#Creating menu button in Tkinter
+# def menuButtonInTinker():
+#     top = Tk()
+#     mb = Menubutton( top, text = &quot;GfG&quot;)
+#     mb.grid()
+#     mb.menu = Menu ( mb, tearoff=0)
+#     mb[&quot;menu&quot;] = mb.menu
+#     cVar = IntVar()
+#     aVar = IntVar()
+#     mb.menu.add_checkbutton(label='Contact', variable=cVar)
+#     mb.menu.add_checkbutton(label='About', variable=aVar)
+#     mb.pack()
+#     top.mainloop()
+
+#Creating Menu in Tkinter
+def menuInTinker():
+    root = Tk()
+    menu = Menu(root)
+    root.config(menu=menu)
+    filemenu = Menu(menu)
+    menu.add_cascade(label='File', menu=filemenu)
+    filemenu.add_command(label='New')
+    filemenu.add_command(label='Open...')
+    filemenu.add_separator()
+    filemenu.add_command(label='Exit', command=root.quit)
+    helpmenu = Menu(menu)
+    menu.add_cascade(label='Help', menu=helpmenu)
+    helpmenu.add_command(label='About')
+    mainloop()
+
+#Creating Message in Tkinter
+def messageinTinker():
+    main = Tk()
+    ourMessage = 'This is our Message'
+    messageVar = Message(main, text=ourMessage)
+    messageVar.config(bg='lightgreen')
+    messageVar.pack()
+    main.mainloop()
+
+#Creating Radiobutton in Tkinter
+def radioButtonInTkinter():
+    root = Tk()
+    v = IntVar()
+    Radiobutton(root, text='GFG', variable=v, value=1).pack(anchor=W)
+    Radiobutton(root, text='MIT', variable=v, value=2).pack(anchor=W)
+    mainloop()
+
+#Creating Scale in Tkinter
+def scaleInTkinter():
+    master = Tk()
+    w = Scale(master, from_=0, to=42)
+    w.pack()
+    w = Scale(master, from_=0, to=200, orient=HORIZONTAL)
+    w.pack()
+    mainloop()
+
+#Creating a Scrollbar in Tkinter
+def scrollBarInTkinter():
+    root = Tk()
+    scrollbar = Scrollbar(root)
+    scrollbar.pack(side=RIGHT, fill=Y)
+    mylist = Listbox(root, yscrollcommand=scrollbar.set)
+    for line in range(100):
+        mylist.insert(END, 'This is line number' + str(line))
+    mylist.pack( side = LEFT, fill = BOTH)
+    scrollbar.config( command = mylist.yview)
+    mainloop()
+
+#Creating Text in Tkinter
+def textInTkinter():
+    root = Tk()
+    T = Text(root, height=2, width=30)
+    T.pack()
+    T.insert(END, 'GeekforGeeks\nBEST WEBSITE\n')
+    mainloop()
+
+#Creating Toplevel in Tkinter
+def toplevelInTkinter():
+    root = Tk()
+    root.title('GFG')
+    top = Toplevel()
+    top.title('Python')
     top.mainloop()
 
+#Creating Spinbox in Tkinter
+def spinboxInTkinter():
+    master = Tk()
+    w = Spinbox(master, from_=0, to = 10)
+    w.pack()
+    mainloop()
+
+#Creating panned Window in tkinter
+def pannedwindowInTkinter():
+    m1 = PanedWindow()
+    m1.pack(fill = BOTH, expand = 1)
+    left = Entry(m1, bd = 5)
+    m1.add(left)
+    m2 = PanedWindow(m1, orient = VERTICAL)
+    m1.add(m2)
+    top = Scale( m2, orient = HORIZONTAL)
+    m2.add(top)
+    mainloop()
+
+
+pannedwindowInTkinter()
 
 
 
