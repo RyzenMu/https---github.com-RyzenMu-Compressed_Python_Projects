e34dce451ce545107be07177851769a754a98c87
RyzenMu <106828992+RyzenMu@users.noreply.github.com>
2022-10-07 09:06:38 +0530

    Update Story_Teller.py

--- Story_Teller.py
+++ Story_Teller.py
@@ -1,3 +1,4 @@
+#
 # Did you think the most complex way of using a random module in python is random sampling? This idea cannot be farther from the truth. It is also possible to create random stories and even beyond using the random module. 
 
 # Here, the aim is to create a random story each time the user runs the program. The story will be the same always but with little variation with the input. This is a fun but exciting python project which will work wonders with beginners to gain confidence in python.
